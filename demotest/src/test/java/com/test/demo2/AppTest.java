4d12103c50d043e36d7cd4c8663a80ccbb984d83
sugramal <sugramal@SUGRAMAL-EGE3J.partnet.cisco.com>
2017-06-20 20:35:00 +0530

    commit first

--- demotest/src/test/java/com/test/demo2/AppTest.java
+++ demotest/src/test/java/com/test/demo2/AppTest.java
@@ -7,32 +7,10 @@ import junit.framework.TestSuite;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+public class AppTest {
+	
+	public void test()
+	{
+		System.out.println("Hi I am sugi...");
+	}
 }
